1009259c7abf6d04df90c36f64b9f4db56d6c3b8
Rye Welz Geselowitz <ryewelzgeselowitz@Ryes-MacBook-Pro.local>
2023-09-21 21:05:54 -0400

    add transaction scraping

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 from creds import USERNAME, PASSWORD
-from scraper import scrape_benefits
+from scraper import scrape_benefits, scrape_transactions
+import os 
+from datetime import datetime
+import csv
 
-USE_HEADLESS_DRIVER = True 
+USE_HEADLESS_DRIVER = bool(int(os.environ.get('USE_HEADLESS_DRIVER', 1))) 
 
 
 def scrape(username, password):
-    benefits = scrape_benefits(username, password, USE_HEADLESS_DRIVER)
-    for benefit in benefits:
-        print(f'\n{benefit["name"]}: Issued {benefit["issued"]} {benefit["unit"]}, remaining {benefit["remaining"]} {benefit["unit"]}')
+    # benefits = scrape_benefits(username, password, USE_HEADLESS_DRIVER)
+    # for benefit in benefits:
+    #     print(f'\n{benefit["name"]}: Issued {benefit["issued"]} {benefit["unit"]}, remaining {benefit["remaining"]} {benefit["unit"]}')
+
+    start_dt = datetime.now()
+    transactions = scrape_transactions(username, password, USE_HEADLESS_DRIVER)
+
+    with open('transactions.csv', 'w') as csvfile:
+        fieldnames = transactions[0].keys()
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for transaction in transactions:
+            writer.writerow(transaction)
+
+    print(f'It took {datetime.now() - start_dt}')
+
+
 
 
 if __name__ == '__main__':
--- scraper.py
+++ scraper.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
+import concurrent.futures
 
 LOGIN_PAGE_URL = 'https://www.wicconnect.com/wicconnectclient/siteLogonClient.recip?state=NEW%20YORK%20WIC&stateAgencyId=1'
 
@@ -62,15 +63,100 @@ def get_benefits_from_html(html_doc):
     
     return benefits
 
+def _get_transactions_from_html(html_doc):
+    soup = BeautifulSoup(html_doc, 'html.parser')
+    table = soup.find('table', class_='sort-table')
+    if table is None:
+        return []
+
+    transactions = []
+
+    trs = []
+    for class_name in ['RowEven', 'RowOdd']:
+        trs += table.find_all('tr', attrs={'class': class_name})
+    for tr in trs:
+        tds = tr.find_all('td')
+        transaction = {
+            'date': tds[0].text.strip(),
+            'quantity': float(tds[1].text.strip()),
+            'unit': tds[2].text.strip(),
+            'item': tds[3].text.strip(),
+            'transaction': tds[4].text.strip(),
+            'location': tds[5].text.strip()
+        }
+        transactions.append(transaction)
+    
+    return transactions
+
+
+def _get_year_options(driver):
+    year_input = driver.find_element(By.NAME, "year")
+    year_input.click()
+    year_options = year_input.find_elements(By.TAG_NAME, "option")  
+    return year_options
+
+def _get_month_options(driver):
+    month_input = driver.find_element(By.NAME, "month")
+    month_input.click()
+    month_options = month_input.find_elements(By.TAG_NAME, "option")    
+    return month_options
+
+def _get_transactions(driver):
+    transactions = []
+    year_idx = 0 
+    month_idx = 0
+    while True:
+        # Select year
+        year_options = _get_year_options(driver)
+        year_options[year_idx].click()
+
+        # Select month
+        month_options = _get_month_options(driver)
+        month_options[month_idx].click()
+
+        # Search
+        search_button = driver.find_element(By.NAME, 'app_search_button')
+        search_button.click()
+
+        WebDriverWait(driver, 3).until(
+            EC.url_contains(('trxhistory.recip'))
+        )
+
+        # grab transactions
+        transactions += _get_transactions_from_html(driver.page_source)
+
+        menu_links = driver.find_elements(By.CLASS_NAME, "menu_link")
+        balance_link = menu_links[0]
+        assert 'future balance' in balance_link.get_attribute('innerHTML').lower()
+        balance_link.click()
+
+        # Why isn't this working?!
+        WebDriverWait(driver, 3).until(
+            EC.url_contains(('main.recip'))
+        )
+
+        if month_idx < len(month_options) - 1:
+            month_idx += 1
+        elif year_idx < len(year_options) - 1:
+            year_idx += 1
+            month_idx = 0
+        else:
+            break
+
+    return transactions
+
 
 def scrape_benefits(username, password, use_headless_driver=True):
     driver = get_driver(use_headless_driver)
+
+    # Log in 
     try:
         login(driver, username, password)
     except Exception as e:
         driver.quit()
         raise LoginException(e)
     
+    # scrape_benefits 
     html_doc = driver.page_source
     try:
         benefits = get_benefits_from_html(html_doc)
@@ -80,3 +166,25 @@ def scrape_benefits(username, password, use_headless_driver=True):
 
     driver.quit()
     return benefits
+
+
+def scrape_transactions(username, password, use_headless_driver=True):
+    driver = get_driver(use_headless_driver)
+    # Log in 
+    try:
+        login(driver, username, password)
+    except Exception as e:
+        driver.quit()
+        raise LoginException(e)
+    
+    # Scrape transactions
+    try:
+        transactions = _get_transactions(driver)
+    except Exception as e:
+        html_doc = driver.page_source
+        driver.quit()
+        raise ScrapingException(error=e, html_doc=html_doc)
+
+    driver.quit()
+    return transactions
+
