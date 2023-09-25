22c22c741562819bfc02d06a3eab78ee29a559bf
Rye Welz Geselowitz <ryewelzgeselowitz@Ryes-MacBook-Pro.local>
2023-09-25 13:16:14 -0400

    clean up migration history

--- migrations/1-add_logs_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE endpoint_logs (
-	id SERIAL PRIMARY KEY,
-    username text not null,
-	when_created timestamp NOT NULL default now(),
-    endpoint text not null,
-  	status text not null,
-    error_desc text,
-  	html_doc text
-);
